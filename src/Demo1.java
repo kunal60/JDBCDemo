f43eb94da445d08892c6f245c9e2b4b3bfb6e24c
kunal.malhotra <kunal.malhotra@Kunalmalhotra9322@gmail.com>
2018-01-19 12:06:38 +0530

    Added Comments

--- src/Demo1.java
+++ src/Demo1.java
@@ -6,11 +6,14 @@ import java.sql.Statement;
 public class Demo1 {
 
 	public static void main(String[] args) {
-
+		// http://www.studytonight.com/java/steps-to-connect-to-database-in-java.php
+		// https://www.javatpoint.com/example-to-connect-to-the-oracle-database
 		try {
+
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			// http://www.techpaste.com/2011/07/java-sql-sqlexception-listener-refused-connection-error/
 			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@172.18.73.194:1521:XE", "system", "oracle");
+			//http://www.studytonight.com/java/connecting-to-oracle.php
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from emp");
 
